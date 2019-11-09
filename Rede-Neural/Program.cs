c74aa0ab670bf14865bab8a4b87b3935e66252aa
Luiz Murakami <luizhm.murakami@univem.edu.br>
2019-11-09 10:04:15 -0300

    Inicialização do projeto

---
+++ Rede-Neural/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rede_Neural
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
